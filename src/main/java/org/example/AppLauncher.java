a21dfa84258bc2024c3cdbb67bd5eb6ad4fabe12
ruslana <ruslana.prus@ukr.net>
2024-08-18 23:20:33 +0200

    add launch to control whichever numberGetter is needed

--- src/main/java/org/example/AppLauncher.java
+++ src/main/java/org/example/AppLauncher.java
@@ -9,16 +9,17 @@ public class AppLauncher {
     private static final Logger logger = LoggerFactory.getLogger(AppLauncher.class);
 
     public static void main(String[] args) {
+        AppLauncher appLauncher = new AppLauncher();
+        appLauncher.launch(new RandomInt(), new SumCalculator());
+        appLauncher.launch(NumberFromFile.getInstance("src/main/resources/number.txt"), new SumCalculator());
+        appLauncher.launch(new UserInput(), new SumCalculator());
+    }
+
+    public void launch(NumberGetter numberGetter, SumCalculator sumCalculator) {
         try {
-            NumberGetter userInput = new UserInput();
-            NumberManager numberManager = new NumberManager(userInput);
-//        NumberGetter randomInt = new RandomInt();
-//        NumberManager numberManager = new NumberManager(randomInt);
-//            NumberGetter numberFromFile = NumberFromFile.getInstance("src/main/resources/number.txt");
-//            NumberManager numberManager = new NumberManager(numberFromFile);
+            NumberManager numberManager = new NumberManager(numberGetter);
             int number = numberManager.getNumericValue();
             logger.info("Retrieved number: {}", number);
-            SumCalculator sumCalculator = new SumCalculator();
             logger.info("Sum of numbers from 1 to {}: {}", number, sumCalculator.sum(number));
         } catch (IllegalStateException | IllegalArgumentException e) {
             logger.error("Error: {}", e.getMessage(), e);
